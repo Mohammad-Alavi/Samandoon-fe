d94c0788518d56f6cebc5c7b0b8d61026ca945a8
MosleM <moslem.deris@gmail.com>
2018-05-13 04:04:43 +0430

    Added api services as providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './routes/index-page/index-page.component';
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { ArticlePageComponent } from './routes/article-page/article-page.component';
+import { NgoApiService } from './services/api/ngo/ngo.api.service';
+import { ArticleApiService } from './services/api/article/article.api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { EventApiService } from './services/api/event/event.api.service';
 
 @NgModule({
   declarations: [
@@ -15,8 +19,13 @@ import { ArticlePageComponent } from './routes/article-page/article-page.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+      NgoApiService,
+      ArticleApiService,
+      EventApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
