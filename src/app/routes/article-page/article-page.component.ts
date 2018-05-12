cb61a55172f355b54500f662c48f3f0dd018b87f
MosleM <moslem.deris@gmail.com>
2018-05-13 04:06:04 +0430

    Added an example of how to inject and use the api service

--- src/app/routes/article-page/article-page.component.ts
+++ src/app/routes/article-page/article-page.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {ArticleApiService} from '../../services/api/article/article.api.service';
+import {ArticleApiInterface} from '../../services/api/article/article.api.interface';
+import {Article} from '../../objects/article';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article-page.component.html',
   styleUrls: ['./article-page.component.scss']
 })
-export class ArticlePageComponent implements OnInit {
+export class ArticlePageComponent implements OnInit, ArticleApiInterface {
 
-  constructor(private route: ActivatedRoute ) { }
+  article: Article;
+
+  constructor(private route: ActivatedRoute,
+              private articleApiService: ArticleApiService) { }
 
   ngOnInit() {
+    this.getArticle();
   }
 
   getArticle() {
     const id = this.route.snapshot.paramMap.get('article_id');
+    this.articleApiService.getArticle(id, this);
+  }
+
+  //  This method is called whenever we call getArticle() and the response arrives
+  onArticleLoaded(article: Article) {
+    this.article = article;
   }
 
 }
