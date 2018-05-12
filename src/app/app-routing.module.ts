4c18890a26e3b6feffa789fa522b41fd7008dbe5
MosleM <moslem.deris@gmail.com>
2018-05-13 02:53:37 +0430

    Added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Route, RouterModule} from '@angular/router';
-import {AppComponent} from './app.component';
+import {IndexPageComponent} from './routes/index-page/index-page.component';
+import {ArticlePageComponent} from './routes/article-page/article-page.component';
 
 const routes: Route[] = [
-    {path: '', component: AppComponent}
+    {path: '', component: IndexPageComponent},
+    {path: 'article/:article_id', component: ArticlePageComponent}
 ];
 
 @NgModule({
