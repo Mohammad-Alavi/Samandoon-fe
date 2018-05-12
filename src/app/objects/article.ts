6d7b588f6318eef010e26b62bb498ce0f006c589
MosleM <moslem.deris@gmail.com>
2018-05-13 03:06:46 +0430

    Added object

---
+++ src/app/objects/article.ts
@@ -0,0 +1,7 @@
+export class Article {
+    id: string;
+    title: string;
+    like_count: number;
+    comment_count: number;
+    seen_count: number;
+}
---
+++ src/app/objects/ngo.ts
@@ -0,0 +1,10 @@
+export class Ngo {
+    id: string;
+    name: string;
+    description: string;
+    area_of_activity: string;
+    status: string;
+    zip_code: string;
+    type: string;
+    confirmed: boolean;
+}
