d28b80b5c9686d526d3d51686911fcffc9fea701
MosleM <moslem.deris@gmail.com>
2018-05-14 02:58:02 +0430

    Minor changes

--- src/app/routes/article-page/article-page.component.ts
+++ src/app/routes/article-page/article-page.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ArticleApiService} from '../../services/api/article/article.api.service';
 import {ArticleApiInterface} from '../../services/api/article/article.api.interface';
 import {Article} from '../../objects/article';
 
 @Component({
-  selector: 'app-article',
-  templateUrl: './article-page.component.html',
-  styleUrls: ['./article-page.component.scss']
+    selector: 'app-article',
+    templateUrl: './article-page.component.html',
+    styleUrls: ['./article-page.component.scss']
 })
 export class ArticlePageComponent implements OnInit, ArticleApiInterface {
 
-  article: Article;
+    article: Article;
 
-  constructor(private route: ActivatedRoute,
-              private articleApiService: ArticleApiService) { }
+    constructor(private route: ActivatedRoute,
+                private articleApiService: ArticleApiService) {
+    }
 
-  ngOnInit() {
-    this.getArticle();
-  }
+    ngOnInit() {
+        this.getArticle();
+    }
 
-  getArticle() {
-    const id = this.route.snapshot.paramMap.get('article_id');
-    this.articleApiService.getArticle(id, this);
-  }
+    getArticle() {
+        const id = this.route.snapshot.paramMap.get('article_id');
+        this.articleApiService.getArticle(id, this);
+    }
 
-  //  This method is called whenever we call getArticle() and the response arrives
-  onArticleLoaded(article: Article) {
-    this.article = article;
-  }
+    //  This method is called whenever we call getArticle() and the response arrives
+    onArticleLoaded(article: Article) {
+        this.article = article;
+    }
 
 }
