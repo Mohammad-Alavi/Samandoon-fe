66c86282f1b53405511563ca21cc3d1fb61d6bdb
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-05-14 11:32:13 +0430

    Added ng-bootstrap and bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './routes/index-page/index-page.component';
@@ -23,7 +24,8 @@ import { NgoPageComponent } from './routes/ngo-page/ngo-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
   providers: [
       NgoApiService,
