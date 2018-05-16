5dd85a23051589a4e368018660f9398af274e44e
MosleM <moslem.deris@gmail.com>
2018-05-16 16:54:42 +0430

    Added `ngo` field to Event model

--- src/app/services/api/event/event.api.service.ts
+++ src/app/services/api/event/event.api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EventApiInterface } from './event.api.interface';
 import { ApiConfig } from '../api.config';
+import { Event } from '../../../objects/event';
 
 @Injectable()
 export class EventApiService {
@@ -9,10 +10,12 @@ export class EventApiService {
   }
 
   getEvent(event_id: string, eventApiInterface: EventApiInterface) {
-    const response = this.http.get(ApiConfig.API_URL + '/ngo/event/' + event_id);
-    response.subscribe(object =>
-      eventApiInterface.onEventLoaded(object['data']['object'])
-    );
+    const response = this.http.get(ApiConfig.API_URL + '/ngo/event/' + event_id + '?include=ngo');
+    response.subscribe(object => {
+      const event: Event = object['data']['object'];
+      event.ngo = object['data']['ngo']['data']['object'];
+      eventApiInterface.onEventLoaded(event);
+    });
   }
 
 }
