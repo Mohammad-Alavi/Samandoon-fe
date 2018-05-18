6fbe09a28b70610092cc8b04f3bb19b5ed9d8b30
MosleM <moslem.deris@gmail.com>
2018-05-18 18:27:51 +0430

    Add pipes for `day`

---
+++ src/app/pipes/date/date-persian-day-letter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'jalali-moment';
+import { Date } from '../../objects/date';
+/*
+ * Converts the date object to first letter of persian day of the week
+ * Usage:
+ *   value | datePersianDayLetter
+ * Example:
+ *   {{ article.created_at | datePersianDayLetter }}
+ *   formats to: پ
+*/
+@Pipe({
+  name: 'datePersianDayLetter'
+})
+export class DatePersianDayLetterPipe implements PipeTransform {
+
+  transform(value: Date, args?: any): string {
+    const jalali = moment(value.date, 'YYYY-M-D HH:mm:ss.000000');
+    return jalali.locale('fa').format('dd');
+  }
+
+}
---
+++ src/app/pipes/date/date-persian-day-name.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Date } from '../../objects/date';
+import * as moment from 'jalali-moment';
+/*
+ * Converts the date object to persian day of the week
+ * Usage:
+ *   value | datePersianDayName
+ * Example:
+ *   {{ article.created_at | datePersianDayName }}
+ *   formats to: پنج‌شنبه
+*/
+@Pipe({
+  name: 'datePersianDayName'
+})
+export class DatePersianDayNamePipe implements PipeTransform {
+
+  transform(value: Date, args?: any): string {
+    const jalali = moment(value.date, 'YYYY-M-D HH:mm:ss.000000');
+    return jalali.locale('fa').format('ddd');
+  }
+
+}
