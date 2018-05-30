5292235ed853a1b0d80bfa381d146e8150fe31d3
MosleM <moslem.deris@gmail.com>
2018-05-30 20:24:03 +0430

    Separate `List` components parent and its logic

--- src/app/views/article-list-view/article-list-view.component.ts
+++ src/app/views/article-list-view/article-list-view.component.ts
@@ -1,19 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Ngo } from '../../objects/ngo';
 import { Article } from '../../objects/article';
 import { ArticleApiService } from '../../services/api/article/article.api.service';
-import { Pageable } from '../Pageable';
+import { List } from '../List';
 
 @Component({
   selector: 'app-article-list-view',
   templateUrl: './article-list-view.component.html',
   styleUrls: ['./article-list-view.component.scss']
 })
-export class ArticleListViewComponent implements OnInit, ApiCallGetAll<Article>, Pageable {
+export class ArticleListViewComponent extends List<Article> {
   private _ngo: Ngo;
-  public page = 1;
-  public isEnded = false;
-  public isLoading = false;
 
   @Input()
   set ngo(ngo: Ngo) {
@@ -25,28 +22,13 @@ export class ArticleListViewComponent implements OnInit, ApiCallGetAll<Article>,
     return this._ngo;
   }
 
-  articleList: Article[] = Array();
-
   constructor(private articleApiService: ArticleApiService) {
-  }
-
-  ngOnInit() {
+    super();
   }
 
   public loadMore() {
     this.isLoading = true;
     this.articleApiService.getArticleList(this.ngo.id, this.page, this);
-  }
-
-  onApiCallSuccess(objList: Article[], currentPage: number, totalPage: number) {
-    objList.forEach((article) => this.articleList.push(article));
-    this.page++;
-    this.isEnded = currentPage >= totalPage;
-    this.isLoading = false;
-  }
-
-  onApiCallFailure() {
-    this.isLoading = false;
   }
 
 }
