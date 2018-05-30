5aefb087c04d73991e416069633dd7a01fcccd75
MosleM <moslem.deris@gmail.com>
2018-05-30 21:42:02 +0430

    Add `Get-Ngo-List` method to api services

--- src/app/services/api/ngo/ngo.api.service.ts
+++ src/app/services/api/ngo/ngo.api.service.ts
@@ -6,10 +6,10 @@ import { Ngo } from '../../../objects/ngo';
 @Injectable()
 export class NgoApiService extends HttpService {
 
-  private getNgoUrl: string = ApiConfig.API_URL + '/ngo/';
+  private getNgoUrl: string = ApiConfig.API_URL + '/ngo';
 
   getNgo(ngo_id: string, callBack: ApiCallGet<Ngo>) {
-    const response = this.http.get(this.getNgoUrl + ngo_id);
+    const response = this.http.get(this.getNgoUrl + '/' + ngo_id);
     response.subscribe(object => {
       callBack.onApiCallSuccess(object['data']['object']);
     }, error => {
@@ -17,4 +17,28 @@ export class NgoApiService extends HttpService {
     });
   }
 
+  getNgoList(page: number, callBack: ApiCallGetAll<Ngo>) {
+    const response = this.http.get(this.getNgoUrl +
+      '?' +
+      'page=' + page +
+      '&' +
+      'orderBy=created_at' +
+      '&' +
+      'sortedBy=desc'
+    );
+    response.subscribe(object => {
+
+      const ngoList: Array<Ngo> = Array();
+      for (const obj of object['data']) {
+        ngoList.push(obj['object']);
+      }
+
+      callBack.onApiCallSuccess(ngoList,
+        object['meta']['pagination']['current_page'],
+        object['meta']['pagination']['total_pages']);
+    }, error => {
+      callBack.onApiCallFailure();
+    });
+
+  }
 }
