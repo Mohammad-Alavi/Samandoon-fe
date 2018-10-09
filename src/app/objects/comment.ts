c813a74f333f7bf66a21740b5efd16389a556cce
MosleM <moslem.deris@gmail.com>
2018-10-09 17:16:21 +0330

    Correct ngo data object

--- src/app/objects/comment.ts
+++ src/app/objects/comment.ts
@@ -5,18 +5,18 @@ export class Comment {
   body: string;
   creator_id: string;
   creator_data: CommentUser;
-  ngo_data: CommentNgo;
   created_at: Date;
   updated_at: Date;
-
-  hasNgo = this.ngo_data != null;
-  hasConfirmedNgo = this.ngo_data != null && this.ngo_data.confirmed === true;
 }
 
 class CommentUser {
   first_name: string;
   last_name: string;
   images: CommentUserAvatar;
+  ngo_data: CommentNgo;
+
+  hasNgo = this.ngo_data != null;
+  hasConfirmedNgo = this.ngo_data != null && this.ngo_data.confirmed === true;
 }
 
 class CommentNgo {
